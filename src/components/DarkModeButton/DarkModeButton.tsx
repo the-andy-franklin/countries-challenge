0f11e066bab928b24ee9e3318477dca941b46142
theandrewfranklin <theandrewfranklin@gmail.com>
2021-12-20 09:37:38 -0800

    configured autofix on save

--- src/components/DarkModeButton/DarkModeButton.tsx
+++ src/components/DarkModeButton/DarkModeButton.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
-import './DarkModeButton.css'
+import './DarkModeButton.css';
 
 const DarkModeButton = () => {
-
-
   return (
     <button className="dark-mode-button">
       <img className="icon" src="assets/moon.svg" alt="oops" />
-      <span className="text">
-        Dark Mode
-      </span>
+      <span className="text">Dark Mode</span>
     </button>
-  )
-}
+  );
+};
 
 export default DarkModeButton;
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -2,19 +2,16 @@ import React from 'react';
 import './NavBar.css';
 
 type Props = {
-  children: JSX.Element
-}
+  children: JSX.Element;
+};
 
-const NavBar = ({children}: Props) => {
-
+const NavBar = ({ children }: Props) => {
   return (
     <div className="nav-bar">
-      <h3>
-        Where in the world?
-      </h3>
+      <h3>Where in the world?</h3>
       {children}
     </div>
-  )
-}
+  );
+};
 
 export default NavBar;

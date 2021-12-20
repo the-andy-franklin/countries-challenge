e2b82cd9fa4f6b2bc76c0f24551cad25cebe510a
theandrewfranklin <theandrewfranklin@gmail.com>
2021-12-20 10:55:11 -0800

    dark mode switching mostly working except svg

--- src/components/DarkModeButton/DarkModeButton.tsx
+++ src/components/DarkModeButton/DarkModeButton.tsx
@@ -2,32 +2,40 @@ import React, { useState } from 'react';
 import './DarkModeButton.css';
 
 const DarkModeButton = () => {
+  const [isDark, setIsDark] = useState(
+    localStorage.getItem('theme') === 'dark',
+  );
+
+  if (isDark) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+  }
+
   const defaultDark =
     window.matchMedia &&
     window.matchMedia('(prefers-color-scheme: dark)').matches;
 
   const setDark = () => {
+    setIsDark((prevState) => !prevState);
     localStorage.setItem('theme', 'dark');
     document.documentElement.setAttribute('data-theme', 'dark');
   };
 
   const setLight = () => {
+    setIsDark((prevState) => !prevState);
     localStorage.setItem('theme', 'light');
     document.documentElement.setAttribute('data-theme', 'light');
   };
 
-  if (defaultDark) {
+  if (defaultDark && !localStorage.getItem('theme')) {
     setDark();
   }
 
   const toggleTheme = () => {
     console.log('going');
-    if (localStorage.getItem('theme') === 'light') {
+    if (!isDark) {
       setDark();
-    } else if (localStorage.getItem('theme') === 'dark') {
+    } else if (isDark) {
       setLight();
-    } else {
-      setDark();
     }
   };
 

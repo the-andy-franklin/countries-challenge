41a7a2e695d7f36fcb203bb8df0831ad735e447a
theandrewfranklin <theandrewfranklin@gmail.com>
2021-12-20 10:39:10 -0800

    dark mode toggle

--- src/components/DarkModeButton/DarkModeButton.tsx
+++ src/components/DarkModeButton/DarkModeButton.tsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './DarkModeButton.css';
 
 const DarkModeButton = () => {
+  const defaultDark =
+    window.matchMedia &&
+    window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+  const setDark = () => {
+    localStorage.setItem('theme', 'dark');
+    document.documentElement.setAttribute('data-theme', 'dark');
+  };
+
+  const setLight = () => {
+    localStorage.setItem('theme', 'light');
+    document.documentElement.setAttribute('data-theme', 'light');
+  };
+
+  if (defaultDark) {
+    setDark();
+  }
+
+  const toggleTheme = () => {
+    console.log('going');
+    if (localStorage.getItem('theme') === 'light') {
+      setDark();
+    } else if (localStorage.getItem('theme') === 'dark') {
+      setLight();
+    } else {
+      setDark();
+    }
+  };
+
   return (
-    <button className="dark-mode-button">
-      <img className="icon" src="assets/moon.svg" alt="oops" />
+    <button className="dark-mode-button" onClick={toggleTheme}>
+      <img className="icon" src="assets/moon.svg" alt="moon" />
       <span className="text">Dark Mode</span>
     </button>
   );
